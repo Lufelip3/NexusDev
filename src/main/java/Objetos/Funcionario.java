49108a9fadae63367c3cf159ed814e04b13e80dc
Lucasmarega <lucasmarega15pedro@gmail.com>
2025-10-28 22:27:14 -0300

    Feito a tela de cadastro de usuário
    foi feito o objeto do usuário, e foi colocado os get e os set

--- src/main/java/Objetos/Funcionario.java
+++ src/main/java/Objetos/Funcionario.java
@@ -9,5 +9,112 @@ package Objetos;
  * @author luis.fmleite
  */
 public class Funcionario {
+    private String Nome;
+    private String CPF;
+    private String Numero;
+    private String CEP;
+    private String Telefone;
+    private String Senha;
+    private String Email;
+
+    /**
+     * @return the Nome
+     */
+    public String getNome() {
+        return Nome;
+    }
+
+    /**
+     * @param Nome the Nome to set
+     */
+    public void setNome(String Nome) {
+        this.Nome = Nome;
+    }
+
+    /**
+     * @return the CPF
+     */
+    public String getCPF() {
+        return CPF;
+    }
+
+    /**
+     * @param CPF the CPF to set
+     */
+    public void setCPF(String CPF) {
+        this.CPF = CPF;
+    }
+
+    /**
+     * @return the Numero
+     */
+    public String getNumero() {
+        return Numero;
+    }
+
+    /**
+     * @param Numero the Numero to set
+     */
+    public void setNumero(String Numero) {
+        this.Numero = Numero;
+    }
+
+    /**
+     * @return the CEP
+     */
+    public String getCEP() {
+        return CEP;
+    }
+
+    /**
+     * @param CEP the CEP to set
+     */
+    public void setCEP(String CEP) {
+        this.CEP = CEP;
+    }
+
+    /**
+     * @return the Telefone
+     */
+    public String getTelefone() {
+        return Telefone;
+    }
+
+    /**
+     * @param Telefone the Telefone to set
+     */
+    public void setTelefone(String Telefone) {
+        this.Telefone = Telefone;
+    }
+
+    /**
+     * @return the Senha
+     */
+    public String getSenha() {
+        return Senha;
+    }
+
+    /**
+     * @param Senha the Senha to set
+     */
+    public void setSenha(String Senha) {
+        this.Senha = Senha;
+    }
+
+    /**
+     * @return the Email
+     */
+    public String getEmail() {
+        return Email;
+    }
+
+    /**
+     * @param Email the Email to set
+     */
+    public void setEmail(String Email) {
+        this.Email = Email;
+    }
+    
+    
     
 }
