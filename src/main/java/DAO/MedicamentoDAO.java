e9a8d4e74301590a13470b84fb5bee6366dd4c8b
Andrey Munhoz <andreymunhoz75@gmail.com>
2025-10-28 22:26:40 -0300

    Tentaivas
    Tentativa de algo

--- src/main/java/DAO/MedicamentoDAO.java
+++ src/main/java/DAO/MedicamentoDAO.java
@@ -3,11 +3,22 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package DAO;
-
-/**
- *
- * @author luis.fmleite
- */
-public class MedicamentoDAO {
     
+import BD.Conexao;
+import Obejtos.Produto;
+import com.mysql.cj.protocol.Resultset;
+import com.mysql.cj.xdevapi.PreparableStatement;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.JOptionPane;
+
+public class MedicamentoDAO {
+    Connection con = Conexao.getConnection();
+    PreparedStatement stmt = null;
+    Resultset rs = null;
+    List<Medicamento> medicamentos 
 }
