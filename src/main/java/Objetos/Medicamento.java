764c45bd50660c6266cb673d7234c4dc68a34c2f
Andrey Munhoz <andreymunhoz75@gmail.com>
2025-11-03 22:20:24 -0300

    Tentando Fazer algumas coisas
    Instanciando painéis e boões.

--- src/main/java/Objetos/Medicamento.java
+++ src/main/java/Objetos/Medicamento.java
@@ -16,4 +16,8 @@ public class Medicamento {
     private String dataValidadeMed;
     private int quantEstoqueMed;
     private Double valorMed;
+
+    public void setVisible(boolean b) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
 }
